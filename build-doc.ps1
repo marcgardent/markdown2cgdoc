141619d826d921f7316a8dc3f419cef8be717efe
marcgardent <marcgardent@yahoo.fr>
2021-01-03 15:26:44 +0100

    doc moved

--- build-doc.ps1
+++ build-doc.ps1
@@ -1,2 +1,2 @@
 
-. "$PSScriptRoot/markdown2cg.ps1" -Source "$PSScriptRoot/samples/war-of-the-eight/war-of-the-eight.md" -ReviewDestination "$PSScriptRoot/samples/war-of-the-eight/review" -ReleaseDestination "$PSScriptRoot/samples/war-of-the-eight/release";
+. "$PSScriptRoot/markdown2cg.ps1" -Source "$PSScriptRoot/samples/war-of-the-eight/war-of-the-eight.md" -ReviewDestination "$PSScriptRoot/docs/war-of-the-eight";

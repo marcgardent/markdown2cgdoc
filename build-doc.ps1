42fb385e63aed6df16df550f1b3c69d5d4b0cc2d
marcgardent <marcgardent@yahoo.fr>
2021-01-03 15:31:46 +0100

    layout converted

--- build-doc.ps1
+++ build-doc.ps1
@@ -1,2 +1,3 @@
 
 . "$PSScriptRoot/markdown2cg.ps1" -Source "$PSScriptRoot/samples/war-of-the-eight/war-of-the-eight.md" -ReviewDestination "$PSScriptRoot/docs/war-of-the-eight";
+. "$PSScriptRoot/markdown2cg.ps1" -Source "$PSScriptRoot/samples/layout.md" -ReviewDestination "$PSScriptRoot/docs/layout" -Leagues 3;
